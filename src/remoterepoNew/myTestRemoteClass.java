8c91e4dd8b182647024f3c9603a71ce8f1e03625
testmanager123 <srinivasavy@gmail.com>
2020-09-20 09:28:42 +0530

    this is testng with shyam

--- src/remoterepoNew/myTestRemoteClass.java
+++ src/remoterepoNew/myTestRemoteClass.java
@@ -5,6 +5,7 @@ public class myTestRemoteClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is new remote options");
+		System.out.println("its update");
 
 	}
 
